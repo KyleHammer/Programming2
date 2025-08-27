b969948b75497f7ff73a0144c7c853ec22190245
Kyle Hammer (Gmail) <kylethomashammer@gmail.com>
2025-08-28 09:14:43 +1000

    Lab 4 Completed Demo

--- Lab 4/Tutor Demo-ZCount - Copy/ZCount.java
+++ Lab 4/Tutor Demo-ZCount - Copy/ZCount.java
@@ -1,5 +1,30 @@
 public class ZCount {
     public static void main(String[] args){
-        
+        String input = readString();
+        int z = zCount(input);
+        System.out.println("z count = " + z);
+    }
+
+    // Top procedure - Level 1
+    // Goal: Reading the string, show the number of 'zs'
+    // read string
+    // output
+    public static String readString(){
+        System.out.print("String: ");
+        return In.nextLine();
+    }
+
+    // Function - Level 2
+    // Goal: Count how many z's are in the string 
+    // count
+    // string loop
+    public static int zCount(String s){
+        int count = 0;
+        for (int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'z' || s.charAt(i) == 'Z'){
+                count++;
+            }
+        }
+        return count;
     }
 }
--- Lab 4/Tutor Demo-ZWordCount - Copy/ZWordCount.java
+++ Lab 4/Tutor Demo-ZWordCount - Copy/ZWordCount.java
@@ -1,5 +1,63 @@
 public class ZWordCount {
+
+    // Top Procedure
+    // Goal: print sentences, show number of words containing z
+    // for-each loop
+    // output
     public static void main(String[] args) {
-        
+        String[] sentences = {
+            "green ideas Zleep furiously",
+            "a zebra graZed",
+            "zorro WAZ zore",
+            "I feel snoozy"
+        };
+
+        for (String sentence : sentences){
+            System.out.println(sentence);
+        }
+        System.out.println("Words containing a 'z' = " + countZ(sentences));
+    }
+
+    // Function level 2
+    //multiple sentences
+    // Goal: Count the total number of z words in all the sentances
+    // Parameters: String[] sentences
+    // return an int (count)
+    // for-each loop
+    // sum
+    public static int countZ(String[] sentences){
+        int sum = 0;
+        for(String sentence : sentences){
+            sum += countZ(sentence);
+        }
+        return sum;
+    }
+
+    // Function level 3
+    // singular sentence
+    // Goal: Count the number of words in a sentence containing a z
+    // Parameters: String sentences
+    // return an int (count)
+    // split the sentence into words
+    // for-each loop
+    public static int countZ(String sentence){
+        String[] words = sentence.split(" +");
+        int count = 0;
+        for(String word : words){
+            if(containsZ(word)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Function level 4
+    // words
+    // Goal: Check if the word contains a Z/z
+    // Parameters: String word
+    // Return a boolean
+    // word.contains("z"), word.contains("Z")
+    public static boolean containsZ(String word){
+        return word.contains("Z") || word.contains("z");
     }
 }

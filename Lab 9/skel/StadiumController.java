a9faeb96fbc6ea17499ebc2bd8306a5ba3ad6034
Kyle Hammer (Gmail) <kylethomashammer@gmail.com>
2025-10-02 10:14:53 +1000

    Lab 9 - 4.1

--- Lab 9/skel/StadiumController.java
+++ Lab 9/skel/StadiumController.java
@@ -8,8 +8,13 @@ import javafx.beans.property.*;
 import java.io.*;
 
 public class StadiumController {
+    @FXML private TextField sellTf;
+
+    private final int getAmount() { return Integer.parseInt(sellTf.getText()); }
+    private final void setAmount(int amount) { sellTf.setText(""+amount); }
 
     @FXML public void handleSell(ActionEvent e){
-        System.out.println("Working");
+        int amount = getAmount();
+        setAmount(0);
     }
 }

ad5324e157e47ecb9f6c0cfca8a05a1119e0f8e8
Kyle Hammer (Gmail) <kylethomashammer@gmail.com>
2025-10-02 10:04:25 +1000

    Lab 9 - 4

--- Lab 9/skel/StadiumController.java
+++ Lab 9/skel/StadiumController.java
@@ -8,4 +8,8 @@ import javafx.beans.property.*;
 import java.io.*;
 
 public class StadiumController {
+
+    @FXML public void handleSell(ActionEvent e){
+        System.out.println("Working");
+    }
 }

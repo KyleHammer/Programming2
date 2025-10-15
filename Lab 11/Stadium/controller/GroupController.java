cf269a628f621f3edd6dde847789c01d55403202
Kyle Hammer (Gmail) <kylethomashammer@gmail.com>
2025-10-16 09:16:28 +1100

    Lab 11 - Improved code

--- Lab 11/Stadium/controller/GroupController.java
+++ Lab 11/Stadium/controller/GroupController.java
@@ -24,11 +24,7 @@ public class GroupController extends Controller<Group> {
             System.out.println("Old Value = " +oldValue+ ", New Value = " +newValue);
             // If new value has digits, sellBtn to enable
             // otherwise disable
-            if(newValue.matches("[0123456789]+")){
-                sellBtn.setDisable(false);
-            }else{
-                sellBtn.setDisable(true);
-            }
+            sellBtn.setDisable(!newValue.matches("[0-9]+"));
 
             // " +" means one or more spaces
             // "[0123456789]" means match 0 or 1 or 2. Single digit

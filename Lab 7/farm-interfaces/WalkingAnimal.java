1ec1b832d622909528f00b22640ba669cb6f7129
Kyle Hammer (Gmail) <kylethomashammer@gmail.com>
2026-03-30 18:11:06 +1100

    Lab 7 - abstract

--- Lab 7/farm-interfaces/WalkingAnimal.java
+++ Lab 7/farm-interfaces/WalkingAnimal.java
@@ -1,6 +1,4 @@
-public class WalkingAnimal implements Animal {
-    public String getName() {return "";}
-    public String getNoise() {return "";}
+public abstract class WalkingAnimal implements Animal {
     public String getMove() {
         return "walks";
     }
